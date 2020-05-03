717df8e581c737265454506473cc8ed135c4242b
Etienne Matteau <etiennematteau@hotmail.fr>
2020-05-03 18:08:57 -0400

    Images retirés et changés et ajout Google map

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,7 +4,7 @@
       <b-col lg="2">
         <img src="../assets/Logov2.svg" class="d-inline-block align-top" alt="Logo complet" height="150px">
       </b-col>
-      <b-col class="info" lg="10" align-self="center">
+      <b-col class="info" lg="7" align-self="center">
         <b-row class="textContainer">
           <p class="whiteText">(581) 996-1001</p>
         </b-row>
@@ -15,6 +15,19 @@
         <b-row class="textContainer">
           <p class="whiteText">490, 5e rang, Val-Alain (Québec) G0S 3H0</p>
         </b-row>
+      </b-col>
+      <b-col lg="3">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2598.703271909711!2d-71.76311238452278!3d46.44787517912495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cb87787d63567dd%3A0x63085f117b5fe562!2s490%205e%20Rang%2C%20Val-Alain%2C%20QC%20G0S%203H0!5e1!3m2!1sfr!2sca!4v1588542148490!5m2!1sfr!2sca"
+          width="300"
+          height="200"
+          frameborder="0"
+          style="border:0;"
+          allowfullscreen=""
+          aria-hidden="false"
+          tabindex="0"
+        >
+        </iframe>
       </b-col>
     </b-row>
     <b-row class="copyright">
