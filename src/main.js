bcebd514c84b6b389fe327af9f609e3a684cf2b9
Etienne Matteau <ematteau@jstrategia.com>
2021-03-19 19:00:51 -0400

    Google analytics

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import App from './App.vue';
 import VueMeta from 'vue-meta';
+import VueAnalytics from 'vue-analytics';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
@@ -10,6 +11,11 @@ Vue.use(VueMeta);
 Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: '',
+  router
+}
 
 new Vue({
   render: h => h(App),
