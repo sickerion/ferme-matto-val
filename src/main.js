6dc7bf310ea96dddbef0efd55ff7c175b4879fa4
Etienne Matteau <ematteau@jstrategia.com>
2021-06-26 13:06:25 -0400

    Messenger

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import App from './App.vue';
 import VueMeta from 'vue-meta';
 import VueAnalytics from 'vue-analytics';
+import VueFbCustomerChat from 'vue-fb-customer-chat';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
@@ -14,6 +15,12 @@ Vue.config.productionTip = false
 
 Vue.use(VueAnalytics, {
   id: 'G-DRK1GBJQRW'
+})
+
+Vue.use(VueFbCustomerChat, {
+  page_id: 112495743738283,
+  theme_color: '#333333',
+  locale: 'fr_CA',
 })
 
 new Vue({
