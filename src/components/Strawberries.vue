e77a185f516a5d6303f94dfd3bece91698485563
Etienne Matteau <ematteau@jstrategia.com>
2022-06-13 19:03:36 -0400

    fin juin

--- src/components/Strawberries.vue
+++ src/components/Strawberries.vue
@@ -5,7 +5,7 @@
         <div class="text-zone">
           <h1 class="title text title-padding">Des fraises pour tous les goûts</h1>
           <p class="text">
-            Que vous les préférez sucrées ou juteuses, grosses ou petites, nos 12 variétés de fraise vous charmerons de la mi-juin à la fin juillet. 
+            Que vous les préférez sucrées ou juteuses, grosses ou petites, nos 12 variétés de fraise vous charmerons de la fin juin à la fin juillet. 
             Venez les choisir vous même en autocueillette ou passez à notre kiosque prendre un casseau bien rempli avec des légumes.
           </p>
         </div>
