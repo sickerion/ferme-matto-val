a8414d3e19ba213dc4df57289788e52fa643db16
Etienne Matteau <ematteau@jstrategia.com>
2023-06-17 09:49:54 -0400

    Ptit projet

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,30 @@
 <script setup>
-  import NavBar from './components/NavBar.vue'
-  import HomeView from './views/HomeView.vue'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <NavBar />
-  <HomeView />
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<script>
-import { useHead } from "@vueuse/head"
-
-export default {
-  setup() {
-      useHead({
-          title: "Ferme Matto-Val - Fraises et légumes biologiques",
-          meta: [
-              {
-                  name: 'description',
-                  content: "Située près de l'autoroutes 20 à 30 minutes de Québec, la Ferme Matto-Val cultive des légumes et des fraises bio. " +
-                    "Autocueillette pour les fraises biologiques en juin et juillet. " +
-                    "Kiosque à la ferme ouvert durant la saison estivale. " +
-                    "Halte gourmande, pique-nique, troupeau de bovins et plaisirs pour toute la famille.",
-              },
-              { property: 'og:title', content: "Ferme Matto-Val - Fraises et légumes biologiques"},
-                {
-                  property: 'og:description',
-                  content: "Située près de l'autoroutes 20 à 30 minutes de Québec, la Ferme Matto-Val cultive des légumes et des fraises bio. " +
-                    "Autocueillette pour les fraises biologiques en juin et juillet. " +
-                    "Kiosque à la ferme ouvert durant la saison estivale."
-                },
-                { property: 'og:image', content: "https://i.imgur.com/bHBFTDX.jpg"},
-                { property: 'og:site_name', content: 'Ferme Matto-Val'},
-                {property: 'og:type', content: 'website'},    
-                {name: 'robots', content: 'index,follow'} 
-          ],
-      });
-  },
-};
-</script>
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,5 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import router from './router'
-import { createHead } from "@vueuse/head"
 
-const head = createHead();
-
-const app = createApp(App);
-
-app.use(head).use(router);
-
-app.mount('#app');
+createApp(App).mount('#app')
