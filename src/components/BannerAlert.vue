6eb69c3e7c78c5ac956b38093cebdbf33b66a320
Etienne Matteau <ematteau@jstrategia.com>
2023-06-30 08:24:16 -0400

    Ajout message autocueillette

--- src/components/BannerAlert.vue
+++ src/components/BannerAlert.vue
@@ -3,7 +3,7 @@
     <input id="banneralert" type="checkbox" class="hidden">
 
     <label class="close cursor-pointer flex items-center justify-between w-full py-2 px-4 bg-green shadow text-white" title="close" for="banneralert">
-      {{ messageOuvertureSansAutocueillette }}
+      {{ messageOuvertureAutocueillette }}
 
       <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
         <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
