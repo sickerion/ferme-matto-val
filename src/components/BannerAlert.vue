04655111b1bd417967ab5bfe31bcee300a4b4958
Etienne Matteau <ematteau@jstrategia.com>
2023-07-17 13:10:51 -0400

    Modification du message pour la fermeture

--- src/components/BannerAlert.vue
+++ src/components/BannerAlert.vue
@@ -3,7 +3,7 @@
     <input id="banneralert" type="checkbox" class="hidden">
 
     <label class="close cursor-pointer flex items-center justify-between w-full py-2 px-4 bg-warning shadow" title="close"  for="banneralert">
-      La récolte 2023 de fraises est terminée. Le kiosque est ouvert le 14 et 15 juillet pour une vente promotionnelle de fraises congelées.
+      {{ closeMessage }}
 
       <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
         <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
