d298359fabcd97a9b35beec01ed3fee0564c3774
Etienne Matteau <ematteau@jstrategia.com>
2024-03-29 11:03:03 -0400

    vue-router retiré

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@ import '@/assets/main.css'
 
 import { createApp } from 'vue'
 import App from '@/App.vue'
-import router from '@/router'
 import { createHead } from "@vueuse/head"
 
 const head = createHead();
 
 const app = createApp(App);
 
-app.use(head).use(router);
+app.use(head);
 
 app.mount('#app');
