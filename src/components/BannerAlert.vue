1246369a7c6a22deca7f1ba95be90ffbeb4abe2e
sickerion <etiennematteau@hotmail.fr>
2024-10-27 09:51:58 -0400

    Update BannerAlert.vue

--- src/components/BannerAlert.vue
+++ src/components/BannerAlert.vue
@@ -3,7 +3,7 @@
     <input id="banneralert" type="checkbox" class="hidden">
 
     <p class="close cursor-pointer flex items-center justify-between w-full py-2 px-4 bg-warning shadow text-black" title="close" for="banneralert">
-      {{ messageOuvertCremerie }}
+      {{ closeMessage }}
 
       <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
         <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
@@ -17,7 +17,7 @@ export default {
   setup() {
     const messageOuvertureSansAutocueillette = "Les fraises sont arrivées! Le kiosque est ouvert. Ouverture de l'autocueillette à venir."
     const messageOuvertureAutocueillette = "Le kiosque et l'autocueillette sont ouverts!"
-    const closeMessage = "La récolte 2024 de fraises est terminée. Merci et à l'année prochaine!"
+    const closeMessage = "La saison est terminée. Merci pour votre fidélité et à l'année prochaine!"
     const messageOuvertCremerie = "La récolte 2024 de fraises est terminée. La crèmerie est ouverte! Horaire sur Facebook.";
     const messageLink = "https://www.facebook.com/fermemattoval/"
 
